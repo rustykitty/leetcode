d1d08804f736acfd77d6d82befd75d4132d62e8a
Rusty <rusty3925@gmail.com>
2024-12-01 21:01:48 -0800

    added explanation for 193

--- 0193-valid-phone-numbers.sh
+++ 0193-valid-phone-numbers.sh
@@ -6,4 +6,10 @@
 # Memory: 3.56 MB (beats 8.13%)
 
 # Read from the file file.txt and output all valid phone numbers to stdout.
-while read REPLY; do [[ $REPLY =~ ^[0-9]{3}-[0-9]{3}-[0-9]{4}$ ]] || [[ $REPLY =~ ^\([0-9]{3}\)\ [0-9]{3}\-[0-9]{4}$ ]] && echo $REPLY; done < file.txt
+while read REPLY; do # read line into $REPLY until EOF
+    [[ $REPLY =~ ^[0-9]{3}-[0-9]{3}-[0-9]{4}$ ]] || # if $REPLY matches regex for nnn-nnn-nnnn
+    [[ $REPLY =~ ^\([0-9]{3}\)\ [0-9]{3}\-[0-9]{4}$ ]] && # if $REPLY matches regex for (nnn) nnn-nnnn
+    echo $REPLY 
+done < file.txt # redirect stdin to read from file.txt
+# all in one line:
+# while read REPLY; do [[ $REPLY =~ ^[0-9]{3}-[0-9]{3}-[0-9]{4}$ ]] || [[ $REPLY =~ ^\([0-9]{3}\)\ [0-9]{3}\-[0-9]{4}$ ]] && echo $REPLY; done < file.txt
