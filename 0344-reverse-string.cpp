7263b3a356b48493d92b8d65980b7b9fc8b46eb0
Rusty <rusty3925@gmail.com>
2024-12-04 15:15:22 -0800

    updated 344

--- 0344-reverse-string.cpp
+++ 0344-reverse-string.cpp
@@ -1,18 +1,19 @@
 /*
 344. Reverse String
 
-Submitted: October 7, 2024
+Submitted: December 4, 2024
 
-Runtime: 115 ms (beats 6.88%)
+Runtime: 0 ms (beats 100.00%)
 Memory: 27.38 MB (beats 18.77%)
 */
 
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        for (int i = 0; i < s.size(); ++i ) {
-            s.insert(s.begin() + i, (char) s.back());
-            s.pop_back();
+        for (int i = 0, n = s.size(); i < n / 2; ++i) {
+            char temp = s[i];
+            s[i] = s[n - i - 1];
+            s[n - i - 1] = temp;
         }
     }
 };
