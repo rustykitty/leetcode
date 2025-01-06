bc1322699242c5d3a2680766e37ae9be37284112
Rusty <rusty3925@gmail.com>
2025-01-05 20:52:44 -0800

    update 287

--- 0287-find-the-duplicate-numer.cpp
+++ 0287-find-the-duplicate-numer.cpp
@@ -3,17 +3,20 @@
 
 Submitted: January 5, 2024
 
-Runtime: 63 ms (beats 20.20%)
-Memory: 87.86 MB (beats 15.89%)
+Runtime: 71 ms (beats 15.48%)
+Memory: 87.77 MB (beats 18.81%)
 */
 
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
-        unordered_map<int, int> map;
+    int findDuplicate(const vector<int>& nums) const noexcept {
+        unordered_set<int> set;
         for (const int n : nums) {
-            map[n]++;
-            if (map[n] == 2) return n;
+            if (set.find(n) == set.end()) {
+                set.emplace(n);
+            } else {
+                return n;
+            }
         }
         return 0;
     }
